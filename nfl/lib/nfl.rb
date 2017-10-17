e0cbef66ba79d155178152053a040ea9aadeb38c
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-17 14:35:28 +0000

    added scraper.rb to begin work on scraping

--- nfl/lib/nfl.rb
+++ nfl/lib/nfl.rb
@@ -1,5 +1,6 @@
 require_relative "./nfl/version.rb"
 require_relative "./nfl/cli.rb"
 require_relative "./nfl/game.rb"
+require_relative "./nfl/scraper.rb"
 
 require_relative '../config/environment'
