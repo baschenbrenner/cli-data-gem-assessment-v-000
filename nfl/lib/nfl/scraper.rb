dab9cde7558dbcdc8b57b98d2c3a11c7aa481879
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-17 14:40:03 +0000

    added language in scraper to begin searching for terms

--- nfl/lib/nfl/scraper.rb
+++ nfl/lib/nfl/scraper.rb
@@ -1,6 +1,23 @@
 class Scraper
 
 
+  def self.scrape_scores_page(index_url)
+     html = File.read(index_url)
+     raw=Nokogiri::HTML(html)
+
+
+     #hometeam
+     #awayteam
+     #hometeamscore
+     #awayteamscore
+     #gamestatus
+     #possession
+     #last_play
+
+
+    
+
+   end
 
 
 end
