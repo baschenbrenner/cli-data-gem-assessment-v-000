dd639a4938d2044a32f631e898edc5be5aa0abb9
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-17 13:24:34 +0000

    addded a working menu in cli.rb

--- nfl/lib/nfl/cli.rb
+++ nfl/lib/nfl/cli.rb
@@ -3,7 +3,61 @@ class NFL::CLI
 
 
   def call
-    puts "scores for the NFL"
+    welcome
+    choose
+    goodbye
+  end
+
+  def welcome
+    puts "Welcome to NFL ScoreFinder. We can print scores of individual games or of all games."
+    puts "Plus you will have an option to dive deeper into individual games to see passing, rushing and receiving leaders!"
+  end
+
+  def choose
+    puts "Are you interested in a single game or seeing all scores?"
+    puts "Enter 1 for single and 2 for all scores or enter 3 for exit"
+    choice = gets
+    while choice != 3
+      if choice.to_i == 1
+      single_game
+      elsif choice.to_i == 2
+        all_games
+      elsif choice.to_i == 3
+        return "Have a great day!"
+      else
+        puts "That is an invalid choice, choose again."
+        choose
+      end
+    puts "Would you like to see more?"
+    choice = gets
+    end
+
+  end
+
+  def deeper_dive
+    passing_leader
+    rushing_leader
+    receiving_leader
+    #option to repeat, see more
+  end
+
+  def single_game
+  end
+
+  def all_games
+  end
+
+  def passing_leader
+  end
+
+  def rushing_leader
+  end
+
+  def receiving_leader
+  end
+
+  def goodbye
+    puts "Sayonara!"
   end
 
 end
